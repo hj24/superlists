598ef6891a0f81c77066f406f7888455b8cd5d81
hj24 <huangjian24@foxmail.com>
2019-03-26 19:23:27 +0800

    用LiveServerTestCase重写了功能测试

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -2,8 +2,9 @@ from selenium import webdriver
 import unittest
 import time
 from selenium.webdriver.common.keys import Keys
+from django.test import LiveServerTestCase
 
-class NewVisitorTest(unittest.TestCase):
+class NewVisitorTest(LiveServerTestCase):
 	"""docstring for NewVisitorTest"""
 	# def __init__(self, arg):
 	# 	super(NewVisitorTest, self).__init__()
@@ -23,7 +24,7 @@ class NewVisitorTest(unittest.TestCase):
 	def test_can_start_a_list_and_retrieve_it_later(self):
 		# kobe 听说有个很酷的在线待办事项应用
 		# 他去看了这个应用的首页
-		self.browser.get('http://localhost:8000')
+		self.browser.get(self.live_server_url)
 		# 他注意到网页的标题和头部都包含”To-Do“这个词
 		self.assertIn('To-Do', self.browser.title)
 		header_text = self.browser.find_element_by_tag_name('h1').text
@@ -55,5 +56,3 @@ class NewVisitorTest(unittest.TestCase):
 		# 页面中有一些文字解说这个功能
 		self.fail('Finish the test!')
 
-if __name__ == '__main__':
-	unittest.main(warnings='ignore')
