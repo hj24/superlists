a8a4fb79e393d0f1f58a5574aed5f88d9f744ba0
hj24 <huangjian24@foxmail.com>
2019-03-26 19:06:06 +0800

    完善请求之后的重定向，并且在模板上显示所有数据

--- lists/views.py
+++ lists/views.py
@@ -1,12 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from lists.models import Item
 
 # Create your views here.
 def home_page(request):
 	"""
+	item.objects.create 是创建Item对象的简化方式无需再掉用.save()方法
+	"""
+	if request.method == 'POST':
+		new_item_text = request.POST['item_text']
+		Item.objects.create(text=new_item_text)
+		return redirect('/')
+	"""
 	render 第三个参数是一个字典，把模板变量的名称映射到值上
 	dict.get(key, default=None) 如果值不在字典中返回默认值
 	"""
-	return render(request, 'home.html', {
-		'new_item_text': request.POST.get('item_text', ''),
-	})
+	items = Item.objects.all()
+	return render(request, 'home.html', {'items': items})
