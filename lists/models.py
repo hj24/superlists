04cd1767f1d7f662306250ae038e59259e39ad29
hj24 <huangjian24@foxmail.com>
2019-04-04 14:20:46 +0800

    使用get_absolute_url 在model中解析URL

--- lists/models.py
+++ lists/models.py
@@ -1,8 +1,11 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
 class List(models.Model):
-	pass
+	
+	def get_absolute_url(self):
+		return reverse('view_list', args=[self.id])
 
 class Item(models.Model):
 	# 定义文本字段
--- lists/views.py
+++ lists/views.py
@@ -23,7 +23,7 @@ def new_list(request):
 		list_.delete()
 		error = "You can't have an empty list item"
 		return render(request, 'home.html', {"error": error})
-	return redirect(f'/lists/{list_.id}/')
+	return redirect(list_)
 
 def view_list(request, list_id):
 	list_ = List.objects.get(id=list_id)
@@ -34,10 +34,10 @@ def view_list(request, list_id):
 			item = Item(text=request.POST['item_text'], list=list_)
 			item.full_clean()
 			item.save()
-			return redirect(f'/lists/{list_.id}/')
+			return redirect(list_)
 		except ValidationError:
 			error = "You can't have an empty list item"
-			
+
 	return render(request, 'list.html', {'list': list_, 'error': error})
 
 
