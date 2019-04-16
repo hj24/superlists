f1652071ec33688e0d5718cdd2dfa9f677c54079
hj24 <huangjian24@foxmail.com>
2019-04-16 19:08:30 +0800

    用装饰器重构辅助函数

--- accounts/authentication.py
+++ accounts/authentication.py
@@ -3,6 +3,8 @@ from accounts.models import User, Token
 
 class PasswordlessAuthenticationBackend(object):
 	# django 2.0以上的版本，定制authenticate要传入request
+	# 在单元测试中相关测试也要传入request，办法如下链接
+	# http://cn.voidcc.com/question/p-pxfipenq-er.html
 	def authenticate(self, request, uid):
 		print('uid', uid, file=sys.stderr)
 		try:
